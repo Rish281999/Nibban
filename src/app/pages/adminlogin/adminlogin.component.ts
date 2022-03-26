dff03d2a3cdc729b7174c62c0ce0454805d26611
Rishabh <thoratrishabh28@gmail.com>
2022-03-26 15:45:32 +0530

    login icon added in user and admin login

--- src/app/pages/adminlogin/adminlogin.component.ts
+++ src/app/pages/adminlogin/adminlogin.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminloginComponent implements OnInit {
 
+  hide = true;
+  username= 'admin';
+  password= 'Admin@123'
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  onSubmit(){
+    if(this.username==='admin' && this.password==='Admin@123'){
+    console.log("welcome");
+    }else{
+      console.log("invalid");
+    }
   }
 
 }
--- src/app/pages/userlogin/userlogin.component.ts
+++ src/app/pages/userlogin/userlogin.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserloginComponent implements OnInit {
 
+  hide = true;
   constructor() { }
 
   ngOnInit(): void {
